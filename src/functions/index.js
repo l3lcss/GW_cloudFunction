62286cbd70825648aa26ebc1f6abfb9ebd5318b1
paretkf <paretk7@gmail.com>
2018-06-08 19:20:39 +0700

    log + transaction

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions"
 const admin = require('firebase-admin')
-import { dateTime } from 'luxon'
+import { DateTime } from 'luxon'
 admin.initializeApp({
     credential: admin.credential.applicationDefault() 
 })
@@ -16,10 +16,12 @@ export let getDiscount = functions.https.onRequest(async (req, res) => {
   let c = await db.collection('promoCode').doc(promoCode).get()
   if (c.exists) {
       let data = c.data()
+      setLog (req.query, state.FIND_CODE, c.data())
       res.send({
         ...data,
         promoCode: promoCode})
   } else {
+      setLog (req.query, state.FIND_CODE, null)
       res.send(null)
   }
 })
@@ -30,26 +32,40 @@ export let checkOut = functions.https.onRequest(async (req, res) => {
     const c = await db.collection('promoCode').doc(promoCode).get()
     if (c.exists && c.data().status === 'unused') {
       if (c.data().type === 'onetime') {
-        await db.collection('promoCode').doc(promoCode).update({
-          status: 'used'
+        // await db.collection('promoCode').doc(promoCode).update({
+        //   status: 'used'
+        // })
+        var promoRef = db.collection('promoCode').doc(promoCode)
+        var transaction = db.runTransaction(t => {
+          return t.get(promoRef).then(doc => {
+            t.update(promoRef, { status: 'used' })
+          })
+        }).then(result => {
+          console.log('Transaction success!')
+        }).catch(err => {
+          console.log('Transaction failure:', err)
         })
         // TODO : DB Transaction + logging
       }
       if (c.data().discount_type === 'Baht') {
         net = net - (c.data().discount_number)
+        setLog (req.query, state.USE_CODE, net)
       } else if (c.data().discount_type === '%') {
         net = net - ((net * c.data().discount_number) / 100)
+        setLog (req.query, state.USE_CODE, net)
       }
+    } else {
+      setLog (req.query, state.USE_CODE, null)
     }
   }
 
   const result = await db.collection('vip').doc(tel).get()
   if (result.exists && net >= 3000) {
     let generatedCode = genCode()
-    let date = dateTime.utc()
-    let expDate = dateTime.utc().plus({ months: 2 })
-    // let date = new Date()
-    // let expDate = new Date()
+    // let date = DateTime.utc()
+    // let expDate = DateTime.utc().plus({ months: 2 })
+    let date = new Date()
+    let expDate = new Date()
     let newCode = {
       create_date: date,
       discount_number: 300,
@@ -59,27 +75,37 @@ export let checkOut = functions.https.onRequest(async (req, res) => {
       type: 'onetime'
     }
     await db.collection('promoCode').doc(generatedCode).set(newCode)
+    setLog (req.query, state.GEN_CODE, {Code : generatedCode, Net : net})
     res.send({ Code : generatedCode, Net : net})
   } else {
+    setLog (req.query, state.GEN_CODE, {Net : net})
     res.send({Net: net})
   }
 })
 
 function genCode() {
-  var code = ''
-  var message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-  for (var i = 0; i < 5; i++) {
-    code += message.charAt(Math.floor(Math.random() * message.length))
+  while (true) {
+    var code = ''
+    var message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    for (var i = 0; i < 5; i++) {
+      code += message.charAt(Math.floor(Math.random() * message.length))
+    }
+    const transaction = db.runTransaction(t => {
+      return t.get(db.collection('promoCode').doc(code))
+    })
+    if (!transaction.exists) {
+      break
+    }
+    // TODO : DB Transaction
   }
-  // TODO : DB Transaction + check DB
   return code
 }
 
-function setLog (raw, state, code) {
+function setLog (raw, state, result) {
   db.collection('logs').add({
       time: new Date(),
       state: state,
-      code: code,
+      result: result,
       raw: raw
   })
 }
