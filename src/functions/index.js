e1510050864c6039c17b1af3bb3f9b192450a3b8
paretkf <paretk7@gmail.com>
2018-06-08 20:14:57 +0700

    updata Date to moment

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions"
 const admin = require('firebase-admin')
-import { DateTime } from 'luxon'
+var moment = require('moment')
 admin.initializeApp({
     credential: admin.credential.applicationDefault() 
 })
@@ -51,41 +51,11 @@ export let checkOut = functions.https.onRequest(async (req, res) => {
     }
   }
 
-    // const c = await db.collection('promoCode').doc(promoCode).get()
-    // if (c.exists && c.data().status === 'unused') {
-    //   if (c.data().type === 'onetime') {
-    //     // await db.collection('promoCode').doc(promoCode).update({
-    //     //   status: 'used'
-    //     // })
-    //     var promoRef = db.collection('promoCode').doc(promoCode)
-    //     var transaction = db.runTransaction(t => {
-    //       return t.get(promoRef).then(doc => {
-    //         t.update(promoRef, { status: 'used' })
-    //       })
-    //     }).then(result => {
-    //       console.log('Transaction success!')
-    //     }).catch(err => {
-    //       console.log('Transaction failure:', err)
-    //     })
-    //     // TODO : DB Transaction + logging
-    //   }
-    //   if (c.data().discount_type === 'Baht') {
-    //     net = net - (c.data().discount_number)
-    //     setLog (req.query, state.USE_CODE, net)
-    //   } else if (c.data().discount_type === '%') {
-    //     net = net - ((net * c.data().discount_number) / 100)
-    //     setLog (req.query, state.USE_CODE, net)
-    //   }
-    // } else {
-    //   setLog (req.query, state.USE_CODE, null)
-    // }
-
   const result = await db.collection('vip').doc(tel).get()
   if (result.exists && net >= 3000) {
     let generatedCode = genCode()
-    // let date = DateTime.utc()
-    // let expDate = DateTime.utc().plus({ months: 2 })
-    let date = new Date()
+    let now = moment().format()
+    let date = moment.parseZone(now).utc().format()
     let expDate = new Date()
     let newCode = {
       create_date: date,
