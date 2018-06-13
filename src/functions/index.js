e3f32ab5b35c2737d6195d4560e22a98a4f2a4c8
princel3lcS <princeboy__@hotmail.com>
2018-06-13 11:22:26 +0700

    copy branch transaction

--- src/functions/index.js
+++ src/functions/index.js
@@ -9,120 +9,145 @@ const state = {
   USE_CODE: 'USE_CODE',
   VALIDATE_DATA: 'VALIDATE_DATA'
 }
+
 const db = admin.firestore()
 
-export let getDiscount = functions.https.onRequest(async (req, res) => {
-  let promoCode = req.query.promoCode
-  let promotion = await db.collection('promoCode').doc(promoCode).get()
-  if (promotion.exists) {
-    let data = promotion.data()
-    setLog (req.query, state.FIND_CODE, data)
+export const getDiscount = functions.https.onRequest(async (req, res) => {
+  const promoCode = req.query.promoCode
+  const promotionDetails = await db.collection('promoCode').doc(promoCode).get()
+  if (promotionDetails.exists) {
+    let promotionData = promotionDetails.data()
     res.send({
-      ...data,
-      promoCode: promoCode
+      ...promotionData,
+      promoCode
     })
   } else {
-    setLog (req.query, state.FIND_CODE, null)
     res.send(null)
   }
 })
 
-export let checkOut = functions.https.onRequest(async (req, res) => {
+export const checkOut = functions.https.onRequest(async (req, res) => {
   let {tel, net, promoCode} = req.query
-  if (!((tel) && (net)) || !(/^\d+$/.test(net))) {
-    setLog (req.query, state.VALIDATE_DATA, 'Bad Values!!!')
-    res.status(400).send('Bad Values!!')
-    return
+  
+  if (!validateDataInput(tel, net)) {
+    res.status(400).send('Bad value!!')
   }
-
+  
   if (promoCode) {
-    let now_Date = new Date()
-    const selectedPromotion = await db.runTransaction(transaction => {
+    const promotion = await db.runTransaction(transaction => {
       return transaction.get(db.collection('promoCode').doc(promoCode))
+      .then (async selectedPromotion => {
+        if (selectedPromotion.exists && selectedPromotion.data().status === 'unused' && (selectedPromotion.data().exp_date > new Date())) {
+          
+          setStatus(selectedPromotion.data().type, promoCode) // set Status to used
+    
+          net = await getNetDiscount (  // set getDiscount Price
+            net,
+            selectedPromotion.data().discount_type,
+            selectedPromotion.data().discount_number
+          )
+
+        } else {
+          // Log Code can not be used.
+        }
+      })
+    }).then(() => {
+      console.log('Transaction used code success') // Log transaction Success
+    }).catch(err => {
+      console.error(err) // Log transaction Error
     })
-
-    if (selectedPromotion.exists && selectedPromotion.data().status === 'unused' && (selectedPromotion.data().exp_date > now_Date)) {
-      await setStatus(selectedPromotion.data().type, promoCode) // set Status to used
-
-      net = getNetDiscount (  // set getDiscount Price
-        net,
-        selectedPromotion.data().discount_type,
-        selectedPromotion.data().discount_number
-      )
-
-      setLog (req.query, state.USE_CODE, net)
-    } else {
-      setLog (req.query, state.USE_CODE, null)
-    }
   }
-
   const result = await getCode(tel, net)
-  setLog (req.query, state.GEN_CODE, result)
   res.send(result)
 })
 
 function getNetDiscount (net, discountType, discountNumber) { // Test
-  if (discountType === 'Baht') {
+  if (discountType === 'amount') {
     net = net - discountNumber
-  } else if (discountType === '%') {
+  } else if (discountType === 'percent') {
     net = net - ((net * discountNumber) / 100)
+  }
+  if (net < 0) {
+    net = 0
   }
   return net
 }
 
-async function setStatus (type, promoCode) {
+function validateDataInput (tel, net) {
+  if (!((tel) && (net)) || !(/^\d+$/.test(net))) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function setStatus (type, promoCode) {
   if (type === 'onetime') {
-    await db.collection('promoCode').doc(promoCode).update({
+    db.collection('promoCode').doc(promoCode).update({
       status: 'used'
     })
   }
 }
 
 async function getCode (tel, net) {
-  const result = await db.collection('vip').doc(tel).get()
-  if (result.exists && net >= 3000) {
-    const generatedCode = genCode()
-    let createDate = new Date()
-    let expDate = new Date(
-      createDate.getFullYear(),
-      createDate.getMonth()+3,
-      createDate.getDay(),
-      createDate.getHours(),
-      createDate.getMinutes(),
-      createDate.getSeconds()
-    )
-    let newCode = {
-      create_date: createDate,
-      discount_number: 300,
-      discount_type: 'Baht',
-      exp_date: expDate,
-      status: 'unused',
-      type: 'onetime'
-    }
-    await db.collection('promoCode').doc(generatedCode).set(newCode)
+  const vip = await db.collection('vip').doc(tel).get()
+  if (vip.exists && net >= 3000) {
+    let generatedCode = genCode()
+    const promotionDocument = await db.runTransaction(transaction => {
+      return transaction.get(db.collection('promoCode').doc('totalDocumentOfPromotionCode'))
+        .then (async totalPromotionCode => {
+          let newDocument = totalPromotionCode.data().promoCode
+          while (newDocument.find(code => code === generatedCode) === generatedCode) {
+            generatedCode = genCode()
+          }
+          newDocument.push(generatedCode)
+          transaction.update(db.collection('promoCode').doc('totalDocumentOfPromotionCode'), { promoCode: newDocument })
+      }).then(async () => {
+        await setNewDocumentPromoCode (generatedCode)
+      }).catch(err => {
+        console.error(err)
+      })
+    })
     return ({ Code : generatedCode, Net : net})
   } else {
     return ({Net: net})
   }
 }
 
+async function setNewDocumentPromoCode (generatedCode) {
+  let createDate = new Date()
+  let expDate = new Date(createDate.getFullYear(), createDate.getMonth()+3, createDate.getDay(), createDate.getHours(), createDate.getMinutes(), createDate.getSeconds())
+  let newCode = {
+    create_date: createDate,
+    discount_number: 300,
+    discount_type: 'amount',
+    exp_date: expDate,
+    status: 'unused',
+    type: 'onetime'
+  }
+  await db.collection('promoCode').doc(generatedCode).set(newCode)
+}
+
 function genCode() { // check Code
   let code
-  let message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  const message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
   do {
-    code = ''
-    for (let i = 0; i < 5; i++) {
-      code += message.charAt(Math.floor(Math.random() * message.length))
-    }
+    code = randomPromoCode (message)
   } while (checkCode(code))
   return code
 }
 
+function randomPromoCode (message) {
+  let code = ''
+    for (let i = 0; i < 5; i++) {
+      code += message.charAt(Math.floor(Math.random() * message.length))
+    }
+  return code
+}
+
 function checkCode (code) {
-  const transaction = db.runTransaction(t => {
-    return t.get(db.collection('promoCode').doc(code))
-  })
-  return transaction.exists
+  const hasPromocode = db.collection('promoCode').doc(code).get()
+  return hasPromocode.exists
 }
 
 function setLog (raw, state, result) {
